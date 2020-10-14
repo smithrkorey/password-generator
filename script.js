cdcf3743ae159ace9367d6ad5e7702c5c53fb9e3
Your Name <koreysmith@Koreys-MacBook-Pro.local>
2020-10-13 22:30:57 -0600

    edits

--- script.js
+++ script.js
@@ -21,13 +21,19 @@ var confirmUpperCase;
 var confirmNumeric;
 var confirmSpecialCharacters;
 
+//variable values
+var lower = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var upper = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var numeric = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+var characters = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "=", "<", ">", "/", ",", "." ]
+
 // Receiving user input
 var confirmLength = parseInt(prompt("Please enter a number to determine the password length. Length must be between 8 and 128 characters):")); 
 
 if (confirmLength > 8 && confirmLength < 128) {
   confirmLowerCase = confirm("Will this contain lowercase letters?");
   confirmUpperCase = confirm("Will this contain uppercase letters?");
-  confirmNumeric = confirm("Will this numbers?");
+  confirmNumeric = confirm("Will this contain numbers?");
   confirmSpecialCharacters = confirm("Will this contain special characters?");
 
 } else {
@@ -35,8 +41,11 @@ if (confirmLength > 8 && confirmLength < 128) {
 }
 
 
+
+
 //Generator Functions
-function getRandomLower() {
+
+/*function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) +97);
 }
 
@@ -45,10 +54,11 @@ function getRandomUpper() {
 }
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 120) + 30);
+  return String.fromCharCode(Math.floor(Math.random() * 120) + 56);
 }
 
 function getRandomCharacter() {
   const symbols = '!@#$%^&*(){}=<>/,.';
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
